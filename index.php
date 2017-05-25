d2aed643766162060c8265c212561262ffab5fbc
cqlhj <cqlhj@live.cn>
2017-05-25 09:32:30 +0800

    新增

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="./css/index.css"/>
+		<title>刘氏家风</title>
+	</head>
+<body>
+	<div id="su">
+		<a href="lurumain.php">
+			<img src="pic/liushijiafen.jpg" alt="刘氏家风"/>
+		</a>
+	</div>
+</body>
+</html>
---
+++ script/index.js
@@ -0,0 +1,7 @@
+var canvas=document.getElementById("myCanvas");
+if (canvas.getContext){
+	var ctx=canvas.getContext("2d");
+	ctx.font="30px 宋体";
+	ctx.fillstyle="blue";
+	ctx.filltext("刘氏家风",50,50);
+}
