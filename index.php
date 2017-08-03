513bcb90bc66cfe125863ff3786987183e8190cd
cqlhj <cqlhj@live.cn>
2017-08-03 12:10:14 +0800

    向index.php添加language

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8"/>
+		<meta http-equiv="Content-Language" content="zh-CN">
 		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0" />
 		<link rel="stylesheet" type="text/css" href="./css/index.css"/>
 		<title>刘氏家风</title>
